e5eed40ca4e9e7b127a671c88d74c032977f2d76
Vadim <fancybear@internet.ru>
2023-06-11 19:40:27 +0400

    fix code style

--- dectohex.c
+++ dectohex.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
   long dec, q, remainder;
   int i, j = 0;
 
   char hex[100];
 
   printf("Enter dec number: ");
-	scanf("%ld", &dec);
+  scanf("%ld", & dec);
 
-	q = dec;
+  q = dec;
 
-  while (q != 0)
-	{
+  while (q != 0) {
     remainder = q % 16;
-        	
-	  if (remainder < 10)
+
+    if (remainder < 10)
       hex[j++] = 48 + remainder;
 
-    else	
-		  hex[j++] = 55 + remainder;
-    
+    else
+      hex[j++] = 55 + remainder;
+
     q /= 16;
 
   }
 
   for (i = j; i >= 0; i--)
     printf("%c", hex[i]);
-  
+
   puts("\n");
-	return 0;
+  return 0;
 }
